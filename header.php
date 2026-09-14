e16d27bd63cc99b7f4fecd2c41a2944428f93245
Rich Wincott <rich.wincott@gmail.com>
2026-09-14 14:15:19 +0100

    update logo and nav items padding

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 		<div class="site-branding">
 			<?php if ( ! is_page( 'home' ) ) : ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/763907261_18003302318978503_5232659097694014864_n.jpg" class="site-logo" alt="<?php bloginfo( 'name' ); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/763907261_18003302318978503_5232659097694014864_n.png" class="site-logo" alt="<?php bloginfo( 'name' ); ?>">
 			</a>
 			<?php endif; ?>
 		</div>
