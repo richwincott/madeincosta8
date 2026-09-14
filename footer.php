fe2b1f4800184eb54a310c4f1a6b73217aa0393a
Rich Wincott <rich.wincott@gmail.com>
2026-09-14 23:38:12 +0100

    fullscreen home hero image

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-    <p><a href="https://www.instagram.com/madeincosta8/" style="display: flex;
+    <div style="display: flex;
         align-items: center;
-        justify-content: center; margin-right: 10px;">
+        justify-content: center; background-color: rgba(255,255,255,0.85);"><a href="https://www.instagram.com/madeincosta8/" style="display: flex;
+        align-items: center;
+        justify-content: center; padding-right: 10px; font-size: 14px;">
             <img src="<?php echo get_template_directory_uri(); ?>/img/instagram_logo_icon_189247-4278989595.png" width="40px" alt="">
-            madeincosta8
-        </a></p>
+            <span style="padding-bottom: 2px;">madeincosta8</span>
+        </a></div>
 
     <?php wp_footer(); ?>
     </body>
