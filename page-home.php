ae047823e3b7846ec2304266d8db351912b07195
Rich Wincott <rich.wincott@gmail.com>
2026-09-15 21:27:18 +0100

    new slogan and intro

--- page-home.php
+++ page-home.php
@@ -1,6 +1,31 @@
 <?php get_header(); ?>
 <div class="home-hero" style="background-image: url('<?php echo esc_url( get_template_directory_uri() . '/img/768615352_18003907130978503_7040886833167885518_n.jpg' ); ?>');">
     <div class="home-hero-overlay"></div>
-    <h2 class="home-hero-logo">Real hands make real clothes.</h2>
+    <div class="home-hero-logo">
+        <h2>Real hands make real clothes</h2>
+        <p> We design and sell digital crochet patterns you make yourself.</p>
+        <a href="<?php echo esc_url( home_url( '/collections' ) ); ?>" style="
+        
+        font-size: 100%;
+    margin: 0;
+    line-height: 1;
+    cursor: pointer;
+    position: relative;
+    text-decoration: none;
+    overflow: visible;
+    padding: .618em 1em;
+    font-weight: 700;
+    border-radius: 3px;
+    left: auto;
+    background-color: #2c2c2c;
+    color: #ebebeb;
+    border: 0;
+    display: inline-block;
+    background-image: none;
+    box-shadow: none;
+    text-shadow: none;
+        
+        ">Explore our collections</a>
+    </div>
 </div>
 <?php get_footer(); ?>
