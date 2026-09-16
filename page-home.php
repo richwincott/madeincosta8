ab3016e560dbce31b92fdca1c07ab89c0a9fd399
Rich Wincott <rich.wincott@gmail.com>
2026-09-16 15:26:14 +0100

    button styling

--- page-home.php
+++ page-home.php
@@ -4,28 +4,12 @@
     <div class="home-hero-logo">
         <h2>Real hands make real clothes</h2>
         <p> We design and sell digital crochet patterns you make yourself.</p>
-        <a href="<?php echo esc_url( home_url( '/collections' ) ); ?>" style="
         
-        font-size: 100%;
-    margin: 0;
-    line-height: 1;
-    cursor: pointer;
-    position: relative;
-    text-decoration: none;
-    overflow: visible;
-    padding: .618em 1em;
-    font-weight: 700;
-    border-radius: 3px;
-    left: auto;
-    background-color: #2c2c2c;
-    color: #ebebeb;
-    border: 0;
-    display: inline-block;
-    background-image: none;
-    box-shadow: none;
-    text-shadow: none;
-        
-        ">Explore our collections</a>
+        <div class="home-cta-container">
+            <a class="home-cta-button" href="<?php echo esc_url( home_url( '/collections' ) ); ?>">
+                Explore our collections
+            </a>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
